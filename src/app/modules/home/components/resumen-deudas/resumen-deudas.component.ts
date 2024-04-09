59ca3ce4304d8d778bedbffab0a074c2a059bb5b
jcvillacobz <jucaviza6@gmail.com>
2024-04-08 23:10:53 -0500

    resumen del saldo

--- src/app/modules/home/components/resumen-deudas/resumen-deudas.component.ts
+++ src/app/modules/home/components/resumen-deudas/resumen-deudas.component.ts
@@ -7,10 +7,10 @@ import { Component } from '@angular/core';
 })
 export class ResumenDeudasComponent {
   cuentas = [
-    { nombre: 'Nequi', valor: '$1,500' },
-    { nombre: 'Davivienda', valor: '$4,200' },
-    { nombre: 'Daviplata', valor: '$1,500' },
-    { nombre: 'Efectivo', valor: '$4,200' },
-    { nombre: 'Bancolombia', valor: '$1,500' }
+    { nombre: 'Nequi', valor: '$1,500', icono: 'fa-solid fa-user' },
+    { nombre: 'Davivienda', valor: '$4,200', icono: 'fa-solid fa-credit-card' },
+    { nombre: 'Daviplata', valor: '$1,500', icono: 'fa-solid fa-user' },
+    { nombre: 'Efectivo', valor: '$4,200', icono: 'fa-solid fa-building-columns' },
+    { nombre: 'Bancolombia', valor: '$1,500', icono: 'fa-solid fa-graduation-cap' }
   ];
 }
--- src/app/modules/home/components/resumen-saldo/resumen-saldo.component.ts
+++ src/app/modules/home/components/resumen-saldo/resumen-saldo.component.ts
@@ -1,127 +1,132 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Chart, registerables } from 'chart.js';
+import 'chartjs-adapter-date-fns';
 
 @Component({
   selector: 'app-resumen-saldo',
   templateUrl: './resumen-saldo.component.html',
-  styleUrls: ['./resumen-saldo.component.scss']
+  styleUrls: ['./resumen-saldo.component.scss'],
 })
 export class ResumenSaldoComponent implements AfterViewInit {
   @ViewChild('chartCanvas') chartCanvas!: ElementRef<HTMLCanvasElement>;
   datos = {
-    "saldoTotal": 3250,
-    "transacciones": [
+    saldoTotal: 3250,
+    transacciones: [
       {
-        "fecha": "2024-03-09",
-        "monto": 500,
-        "tipo": "ingreso"
+        fecha: '2024-03-09',
+        monto: 500,
+        tipo: 'ingreso',
       },
       {
-        "fecha": "2024-03-10",
-        "monto": 200,
-        "tipo": "gasto"
+        fecha: '2024-03-10',
+        monto: 200,
+        tipo: 'gasto',
       },
       {
-        "fecha": "2024-03-11",
-        "monto": 100,
-        "tipo": "ingreso"
+        fecha: '2024-03-11',
+        monto: 100,
+        tipo: 'ingreso',
       },
       {
-        "fecha": "2024-03-12",
-        "monto": 350,
-        "tipo": "gasto"
+        fecha: '2024-03-12',
+        monto: 350,
+        tipo: 'gasto',
       },
       {
-        "fecha": "2024-03-13",
-        "monto": 700,
-        "tipo": "ingreso"
+        fecha: '2024-03-13',
+        monto: 700,
+        tipo: 'ingreso',
       },
       {
-        "fecha": "2024-03-14",
-        "monto": 50,
-        "tipo": "gasto"
+        fecha: '2024-03-14',
+        monto: 50,
+        tipo: 'gasto',
       },
       {
-        "fecha": "2024-03-15",
-        "monto": 100,
-        "tipo": "ingreso"
+        fecha: '2024-03-15',
+        monto: 100,
+        tipo: 'ingreso',
       },
       {
-        "fecha": "2024-03-16",
-        "monto": 150,
-        "tipo": "gasto"
+        fecha: '2024-03-16',
+        monto: 150,
+        tipo: 'gasto',
       },
       {
-        "fecha": "2024-03-17",
-        "monto": 200,
-        "tipo": "ingreso"
+        fecha: '2024-03-17',
+        monto: 200,
+        tipo: 'ingreso',
       },
       {
-        "fecha": "2024-03-18",
-        "monto": 300,
-        "tipo": "gasto"
+        fecha: '2024-03-18',
+        monto: 300,
+        tipo: 'gasto',
       },
       {
-        "fecha": "2024-03-19",
-        "monto": 250,
-        "tipo": "ingreso"
+        fecha: '2024-03-19',
+        monto: 250,
+        tipo: 'ingreso',
       },
       {
-        "fecha": "2024-03-20",
-        "monto": 450,
-        "tipo": "gasto"
+        fecha: '2024-03-20',
+        monto: 450,
+        tipo: 'gasto',
       },
       {
-        "fecha": "2024-03-21",
-        "monto": 150,
-        "tipo": "ingreso"
+        fecha: '2024-03-21',
+        monto: 150,
+        tipo: 'ingreso',
       },
       {
-        "fecha": "2024-03-22",
-        "monto": 200,
-        "tipo": "gasto"
+        fecha: '2024-03-22',
+        monto: 200,
+        tipo: 'gasto',
       },
       {
-        "fecha": "2024-03-23",
-        "monto": 300,
-        "tipo": "ingreso"
+        fecha: '2024-03-23',
+        monto: 300,
+        tipo: 'ingreso',
       },
       {
-        "fecha": "2024-03-24",
-        "monto": 100,
-        "tipo": "gasto"
+        fecha: '2024-03-24',
+        monto: 100,
+        tipo: 'gasto',
       },
       {
-        "fecha": "2024-03-25",
-        "monto": 200,
-        "tipo": "ingreso"
+        fecha: '2024-03-25',
+        monto: 200,
+        tipo: 'ingreso',
       },
       {
-        "fecha": "2024-03-26",
-        "monto": 50,
-        "tipo": "gasto"
+        fecha: '2024-03-26',
+        monto: 50,
+        tipo: 'gasto',
       },
       {
-        "fecha": "2024-03-27",
-        "monto": 400,
-        "tipo": "ingreso"
+        fecha: '2024-03-27',
+        monto: 400,
+        tipo: 'ingreso',
       },
       {
-        "fecha": "2024-03-28",
-        "monto": 100,
-        "tipo": "gasto"
+        fecha: '2024-03-28',
+        monto: 100,
+        tipo: 'gasto',
       },
       {
-        "fecha": "2024-03-29",
-        "monto": 300,
-        "tipo": "ingreso"
+        fecha: '2024-03-29',
+        monto: 300,
+        tipo: 'ingreso',
       },
       {
-        "fecha": "2024-03-30",
-        "monto": 150,
-        "tipo": "gasto"
-      }
-    ]
+        fecha: '2024-03-30',
+        monto: 150,
+        tipo: 'gasto',
+      },
+    ],
+  };
+
+  constructor() {
+    Chart.register(...registerables);
   }
 
   ngAfterViewInit(): void {
@@ -131,27 +136,62 @@ export class ResumenSaldoComponent implements AfterViewInit {
   createLineChart(canvas: ElementRef<HTMLCanvasElement>) {
     const context = canvas.nativeElement.getContext('2d');
     if (context) {
+      // Paso 1: Ordenar las transacciones por fecha en orden descendente.
+      const sortedTransactions = this.datos.transacciones.sort((a, b) =>
+        b.fecha.localeCompare(a.fecha)
+      );
+
+      // Paso 2: Calcular el saldo diario.
+      let saldo = this.datos.saldoTotal;
+      const saldosDiarios = sortedTransactions
+        .map((transaccion) => {
+          if (transaccion.tipo === 'ingreso') {
+            saldo -= transaccion.monto; // Restar al saldo si es ingreso ya que vamos en reversa
+          } else {
+            saldo += transaccion.monto; // Sumar al saldo si es gasto
+          }
+          return { fecha: transaccion.fecha, saldo };
+        })
+        .reverse(); // Invertir para tener los datos en orden cronológico
+
+      // Paso 3: Generar las etiquetas (fechas) y los datos (saldos) para el gráfico
+      const labels = saldosDiarios.map((d) => d.fecha);
+      const data = saldosDiarios.map((d) => d.saldo);
+
+      // Paso 4: Configurar el gráfico
       new Chart(context, {
         type: 'line',
         data: {
-          labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio"],
-          datasets: [{
-            label: 'Dataset de Líneas',
-            data: [65, 59, 80, 81, 56, 55, 40],
-            fill: false,
-            borderColor: 'rgb(75, 192, 192)',
-            tension: 0.1
-          }]
+          labels,
+          datasets: [
+            {
+              label: 'Saldo diario del último mes',
+              data,
+              fill: false,
+              borderColor: 'rgb(75, 192, 192)',
+              tension: 0.1,
+            },
+          ],
         },
         options: {
+          scales: {
+            x: {
+              type: 'time',
+              time: {
+                unit: 'day',
+                tooltipFormat: 'yyyy-MM-dd',
+                displayFormats: {
+                  day: 'yyyy-MM-dd',
+                },
+              },
+            },
+          },
           plugins: {
-            legend: { display: false }
+            legend: { display: false },
           },
           responsive: true,
-        }
+        },
       });
     }
   }
-
-
 }
