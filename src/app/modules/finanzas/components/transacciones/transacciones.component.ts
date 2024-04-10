c9a5d7a30b8317f281121d73ae5957b90a36fa11
jcvillacobz <jucaviza6@gmail.com>
2024-04-09 23:52:20 -0500

    Sin mucho avance

--- src/app/modules/finanzas/components/transacciones/transacciones.component.ts
+++ src/app/modules/finanzas/components/transacciones/transacciones.component.ts
@@ -3,18 +3,50 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-transacciones',
   templateUrl: './transacciones.component.html',
-  styleUrls: ['./transacciones.component.scss']
+  styleUrls: ['./transacciones.component.scss'],
 })
 export class TransaccionesComponent {
   transacciones = [
-    { nombre: 'Natasha Davi', tipo: 'Gasto', fecha: new Date(), cantidad: 75.88 },
-    { nombre: 'Natasha Davi', tipo: 'Ingreso', fecha: new Date(), cantidad: 75.88 },
-    { nombre: 'Natasha Davi', tipo: 'Gasto', fecha: new Date(), cantidad: 75.88 },
-    { nombre: 'Natasha Davi', tipo: 'Ingreso', fecha: new Date(), cantidad: 75.88 },
-    { nombre: 'Natasha Davi', tipo: 'Gasto', fecha: new Date(), cantidad: 75.88 },
-    { nombre: 'Natasha Davi', tipo: 'Ingreso', fecha: new Date(), cantidad: 75.88 },
+    {
+      TransaccionID: 11,
+      CuentaID: 3,
+      CategoriaID: 1,
+      Tipo: 'Gasto',
+      Monto: 50000,
+      Fecha: '2024-04-05T15:00:00.000Z',
+      Descripcion: 'En la tienda de la esquina',
+      NombreCuenta: 'Davivienda',
+      IconoCuenta: 'fa-solid fa-building-columns',
+      NombreCategoria: 'Arriendo',
+      IconoCategoria: 'fa-solid fa-van-shuttle'
+    },
+    {
+      TransaccionID: 11,
+      CuentaID: 3,
+      CategoriaID: 1,
+      Tipo: 'Gasto',
+      Monto: 50000,
+      Fecha: '2024-04-05T15:00:00.000Z',
+      Descripcion: 'En la tienda de la esquina',
+      NombreCuenta: 'Davivienda',
+      IconoCuenta: 'fa-solid fa-building-columns',
+      NombreCategoria: 'Arriendo',
+      IconoCategoria: 'fa-solid fa-van-shuttle'
+    },
+    {
+      TransaccionID: 11,
+      CuentaID: 3,
+      CategoriaID: 1,
+      Tipo: 'Gasto',
+      Monto: 50000,
+      Fecha: '2024-04-05T15:00:00.000Z',
+      Descripcion: 'En la tienda de la esquina',
+      NombreCuenta: 'Davivienda',
+      IconoCuenta: 'fa-solid fa-building-columns',
+      NombreCategoria: 'Arriendo',
+      IconoCategoria: 'fa-solid fa-van-shuttle'
+    }
   ];
-
 
   goBack() {
     window.history.back();
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    apiUrl: 'https://ctapp.coorditanques.com/colaboradoresback/api/v1/pruebas/',
+    dev: true
+};
