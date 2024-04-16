207a6d93d4b1c1af4b417a788c9297ec9f69baf9
jvillacob <jvillacob@coorditanques.com>
2024-04-16 17:07:45 -0500

    Corrigiendo el environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiURL: 'https://ctapp.coorditanques.com/colaboradoresback/api/v1/pruebas/',
+  apiURL: 'https://ctapp.coorditanques.com/colaboradoresback/api/v1/pruebas',
   skipLogin: false
 };
